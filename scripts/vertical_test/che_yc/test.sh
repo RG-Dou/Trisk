8b0ef5fb4269eff9ade38ea2bac529651ea07ba2
RG-Dou <dourg0612@gmail.com>
2023-09-19 20:42:18 +0800

    feat: CacheMissEqn

--- scripts/vertical_test/che_yc/test.sh
+++ scripts/vertical_test/che_yc/test.sh
@@ -1,7 +1,12 @@
 # by default
+
+start() {
+  bash nexmark-run.sh q20 ElasticMemoryManager false 2000 $1 1.0 $2 $3 Random $4
+}
+
 caches="500"
 for i in {1..1}; do
   for cache in $caches; do
-     bash nexmark-run.sh ${i} $cache "clock"
+     start ${i} $cache "LRU" "CacheMissEqn"
 	done
 done
